c6a7980a3a373504704331d77596035890cef8e4
Hammrfall <34660593+Hammrfall@users.noreply.github.com>
2018-03-21 12:02:05 -0400

    update nytimes card

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,29 +1,44 @@
-
 $(document).ready(function () {
   var currentYear = (new Date).getFullYear();
-   $("#endyear").attr("placeholder", "-= " + currentYear)
+  $("#endyear").attr("placeholder", "<= " + currentYear)
 
 
 });
 
+function getArticles() {
+
+  if ($("#topic").val() !== "") {
+    var baseurl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    var topicString = "&q=" +  $("#topic").val();
+    var apiKey = "?api-key=f557c6f93dfb40b7979b9966088ec396"
+    var beginDateString = ""
+    var endDateString = ""
+
+    var checkBegin = $("#startyear").val();
+    if (checkBegin.length == 4 && parseInt(checkBegin) ){
+      beginDateString = "&begin_date=" + checkBegin + "0101"
+    }
+
+    var checkEnd = $("#endyear").val();
+    if (checkEnd.length == 4 && parseInt(checkEnd)){
+      endDateString = "&end_date=" + checkEnd + "1231";
+    }
+    var searchString = baseurl + apiKey + topicString + beginDateString + endDateString;
+    console.log(searchString);
+    $.ajax({
+      url: searchString,
+      method: 'GET',
+
+    }).done(function (result) {
+      console.log(result);
+
+    }).fail(function (err) {
+      throw err;
+    });
+  }
+}
 
 
-
-
-
-//Pull code (tested)
-
-// var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-// var topicString = "&q=Def Leppard"
-// var beginDateString = "&begin_date=19830101"
-// var endDateString = "&end_date=19871231"
-// var apiKey = "?api-key=f557c6f93dfb40b7979b9966088ec396"
-// var searchString = url + apiKey + topicString   + beginDateString + endDateString ;
-// $.ajax({
-//   url: searchString,
-//   method: 'GET',
-// }).done(function(result) {
-//   console.log(result);
-// }).fail(function(err) {
-//   throw err;
-// });
+$("#pullarticlesbutton").click ("click",function(event){
+  getArticles()
+});
